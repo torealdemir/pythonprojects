6cb5a4897840ef7e64066b523872933666e5ea78
torealdemir <aldemirtore@gmail.com>
2023-09-15 17:04:32 +0300

    end of second project!!

--- quiz.py
+++ quiz.py
@@ -2,5 +2,43 @@ print("welcome to my computer quiz")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
+
+print("Lets play!!")
+score = 0
+
+answer = input("What does Cpu stand for? ")
+if answer.lower() == "central processing unit":
+    print("Coorect!")
+    score += 1
+    print(score)
+else:
+    print("Incorrect!")
+
+answer = input("What does gpu stand for? ")
+if answer.lower() == "graphics processing unit":
+    print("Correct!")
+    score += 1
+    print(score)
+else:
+    print("Incorrect!")
+
+answer = input("What does ram stand for? ")
+if answer.lower() == "random access memory":
+    print("Correct!")
+    score += 1
+    print(score)
+else:
+    print("Incorrect")
+
+answer = input("What does psu stand for? ")
+if answer.lower() == "power supply":
+    print("Correct!")
+    score += 1
+    print(score)
+else:
+    print("Incorrect")
+
+
+print("You got " + str(score) + "questions correct!")
